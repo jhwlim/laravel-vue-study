7fe76c45062019f6d813c26dc65d13c70596a929
jh Lim <jhwlim25@gmail.com>
2021-07-19 23:01:43 +0900

    feat(signup): App.vue 헤더 조건 추가, SignupView container 배치

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,30 @@
 <template>
-    <div>
-        <h1>Laravel + Vue Test</h1>
+    <div class="app-container">
+        <h1 v-if="showHeader">Laravel + Vue Test</h1>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
 export default {
-    name: "App.vue"
+    name: "App.vue",
+    data: function() {
+        return {
+            showHeader: false,
+        };
+    },
+    created: function() {
+        const path = this.$route.path;
+        if (path !== '/login' && path !== '/signup') {
+            this.showHeader = true;
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.app-container {
+    width: 100vw;
+    height: 100vh;
+}
 </style>
--- resources/js/views/SignupView.vue
+++ resources/js/views/SignupView.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        Sign up View
+    <div class="signup__wrapper">
+        <div class="signup__container">
+            <h1>회원가입</h1>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.signup__wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+.signup__container {
+
+}
+</style>
