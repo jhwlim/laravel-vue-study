1a86085d9b6732ec746812b9e631dbf9f4e1c3c4
jh Lim <jhwlim25@gmail.com>
2021-07-20 23:48:08 +0900

    feat(signup): 회원 등록시 비밀번호 해시 적용

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,7 +15,7 @@ class UserController extends Controller
     public function store(Request $request) {
         $user = User::create([
             'name' => $request->input('name'),
-            'password' => $request->input('password'),
+            'password' => Hash::make($request->input('password')),
             'email' => $request->input('email'),
         ]);
 
